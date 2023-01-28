6f430407f627f83de361906ce912053d87426ed2
OakmanByte <antonskebaber@gmail.com>
2023-01-28 22:49:35 +0100

    initial commit, just testing diffrent libraries and learning more about serde and serde_json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jwt-rust
-Application for generating custom JSON Web Tokens
+AWS lambda in rust for generating custom JSON Web Tokens
+
+Run test with this command:
+`
+cargo test -- --nocapture <test_name>
+`
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+extern crate serde;
+extern crate serde_json;
+extern crate lambda_runtime;
+
+use std::collections::BTreeMap;
+use hmac::{Hmac, Mac};
+use jwt::SignWithKey;
+use sha2::Sha256;
+use lambda_http::{service_fn, Error};
+use lambda_runtime::LambdaEvent;
+
+use serde::{Deserialize};
+use serde_json::{json, Map, Value};
+
+
+#[derive(Debug, Deserialize)]
+struct ClientRequest {
+    client_id: String,
+    client_secret: String,
+    audience: String,
+    tenant: String,
+    grant_type: String,
+}
+
+/// This is the main body for the function.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    // Extract some useful information from the request
+    let body: Value = event.payload;
+    let client_request: ClientRequest = serde_json::from_value(body).unwrap();
+
+    let key: Hmac<Sha256> = Hmac::new_from_slice(b"some-secret")?;
+    let mut claims = BTreeMap::new();
+    claims.insert("client_id", client_request.client_id);
+    claims.insert("client_secret", client_request.client_secret);
+    claims.insert("audience", client_request.audience);
+    claims.insert("tenant", client_request.tenant);
+    claims.insert("grant_type", client_request.grant_type);
+
+    //Validation
+    let token_str = claims.sign_with_key(&key)?;
+    println!("token:{}", token_str);
+
+    Ok(json!({ "message": format!("token: {}",token_str) }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+#[tokio::test]
+async fn test() {
+    let json_str = r#"{
+        "client_id":"one",
+        "client_secret":"two",
+        "audience": "three",
+        "tenant": "four",
+        "grant_type": "client_credentials"}"#;
+    let input = serde_json::from_str(json_str).expect("failed to parse event");
+    let context = lambda_runtime::Context::default();
+
+    let event = lambda_runtime::LambdaEvent::new(input, context);
+
+    let resp = handler(event).await.unwrap();
+    let expected_response = json!({
+    "message": "token: eyJhbGciOiJIUzI1NiJ9.eyJhdWRpZW5jZSI6InRocmVlIiwiY2xpZW50X2lkIjoib25lIiwiY2xpZW50X3NlY3JldCI6InR3byIsImdyYW50X3R5cGUiOiJjbGllbnRfY3JlZGVudGlhbHMiLCJ0ZW5hbnQiOiJmb3VyIn0.jr1GdFV-n6DmMOEJLGJe3UbkgiO9pHcFBHEI658Q-Ig"
+    });
+    assert_eq!(resp, expected_response)
+}
